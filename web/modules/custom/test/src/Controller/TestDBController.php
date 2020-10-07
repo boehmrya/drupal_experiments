89f13101a9dcb52369f230b6304304dc254f2f62
Ryan <ryan.w.boehm@gmail.com>
2020-10-06 22:46:27 -0500

    added a controller to the test module

---
+++ web/modules/custom/test/src/Controller/TestDBController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\test\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Defines HelloController class.
+ */
+class TestDBController extends ControllerBase {
+
+  /**
+   * Display the markup.
+   *
+   * @return array
+   *   Return markup array.
+   */
+  public function content() {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Hello, World!'),
+    ];
+  }
+
+}
